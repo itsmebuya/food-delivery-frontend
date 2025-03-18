1e1d3a3ad826f321386fe0c9d48ca7506bd94233
24LP7490 24LP7490 <itsmebuya@gmail.com>
2025-03-18 18:07:37 +0800

    qwe

---
+++ src/provider/index.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export const AuthenticationProvider = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        if (!token) {
+            router.push("login");
+        }
+        setLoading(false)
+    }, []);
+
+    if(loading) {
+        return <div>...loading</div>
+    }
+
+    return children
+}
---
+++ src/type/index.tsx
@@ -0,0 +1,59 @@
+import { JSX } from "react"
+
+export type MenuCardProps = {
+    title: string
+    Logo: () => JSX.Element
+    onClick: () => void
+}
+
+export type FoodCardProps = {
+    _id: string
+    category: string
+}
+
+export type User = {
+    _id: string
+    email: string
+    password: string
+    phoneNumber: number
+    address: string
+    role: string 
+    orderedFoods: string
+    ttl: Date
+    isVerified: boolean
+    createdAt: Date
+    updatedAt: Date
+}
+
+export type Food = {
+    _id: string
+    foodName: string
+    price: number 
+    image: string
+    ingredients: string
+    category: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+export type Category = {
+    _id: string
+    categoryName: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+export type FoodOrder = {
+    _id: string
+    user: string
+    totalPrice: number
+    foodOrderItems: string
+    status: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+export type foodOrderItem = {
+    food: string
+    quantity: number
+}
